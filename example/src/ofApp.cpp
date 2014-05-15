34dc0eb7faef9e01db2a32bf900bded4641e78b6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-05-15 15:06:24 -0400

    zoom level 3

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -6,7 +6,7 @@ void ofApp::setup(){
     ofEnableDepthTest();
     
     streetview.setPanoId("y6IoTWYSOZbFBfA1OXCJCA");
-    streetview.setZoom(1);
+    streetview.setZoom(3);
 }
 
 //--------------------------------------------------------------
